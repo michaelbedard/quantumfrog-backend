44fa11e37ace61597627af9c75f2259c8a826f30
courr <courriermichaelbedard@gmail.com>
2024-11-16 22:44:42 -0500

    fix CORS

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
+
 
 from controllers.GameController import game_bp
 from controllers.UserController import home_bp
 from services.UserService import registerUser
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://quantumfrog-frontend-je6u9m75w-michael-bedards-projects.vercel.app/"]}})
 
 app.register_blueprint(home_bp)
 app.register_blueprint(game_bp)
