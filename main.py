f7f80fd94e920ebd3d4b6aeab5e84581e461910c
courr <courriermichaelbedard@gmail.com>
2024-11-16 23:23:27 -0500

    network 2

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ from controllers.UserController import home_bp
 from services.UserService import registerUser
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://quantumfrog-frontend.vercel.app/", "https://quantumfrog-frontend-pduuzbfv6-michael-bedards-projects.vercel.app/", "https://quantumfrog-frontend-git-main-michael-bedards-projects.vercel.app/", "https://quantumfrog-frontend-je6u9m75w-michael-bedards-projects.vercel.app/"], "supports_credentials": True}})
+CORS(app, supports_credentials=True, origins=[
+    "http://localhost:3000",
+    "https://quantumfrog-frontend.vercel.app",
+    "https://quantumfrog-frontend-pduuzbfv6-michael-bedards-projects.vercel.app",
+    "https://quantumfrog-frontend-git-main-michael-bedards-projects.vercel.app",
+    "https://quantumfrog-frontend-je6u9m75w-michael-bedards-projects.vercel.app"
+])
 
 app.register_blueprint(home_bp)
 app.register_blueprint(game_bp)
@@ -20,3 +26,13 @@ if __name__ == '__main__':
         registerUser()
 
     app.run(debug=True)
+
+
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
+    return response
